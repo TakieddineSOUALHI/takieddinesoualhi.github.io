7e77d71a678626977e640741e2219d1ce7c756f1
TakieddineSOUALHI <59415960+TakieddineSOUALHI@users.noreply.github.com>
2026-05-19 15:33:08 +0000

    Deploying to gh-pages from @ TakieddineSOUALHI/takieddinesoualhi.github.io@ee51fbfa6423ac29564ef8cd76d60e88fee6c1cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -411,6 +411,12 @@ ninja.data = [{
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-i-ll-be-attending-the-1st-workshop-on-long-term-deployments-in-the-wild-lowi-held-at-icra26-in-vienna",
